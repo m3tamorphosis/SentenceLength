c98f5aa8c840062ab73295cf68ab1fd9d810ce50
StewartC <StewartC@IT08637>
2023-09-04 11:47:52 +0100

    Add project files.

---
+++ SentenceLength/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input Sentence");
+        string? sen = Console.ReadLine();
+        Console.WriteLine(LongestWord(sen));
+        Console.ReadLine();
+    }
+
+    static string LongestWord(string? sen)
+    {
+        if (string.IsNullOrEmpty(sen))
+        {
+            return "sen is null";
+        }
+        sen = Regex.Replace(sen, @"[^\w\d\s]", "");
+        int senLength = 0;
+        string longest = "";
+        String[] arr = sen.Split(' ');
+        foreach (String s in arr)
+        {
+            if(s.Length > senLength)
+            {
+                senLength = s.Length;
+                longest = s;
+            }
+        }
+        return longest;
+    }
+}
